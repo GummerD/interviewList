5f0bc8b86d1e6d81e8b624ede12c0f3f096cf360
GummerD <denis_gummer@mail.ru>
2024-08-06 15:02:29 +0300

    1. Подключение firebase. Верстка авторизации.
    Регистрация и подключение firebase проходит по адресу
    https://console.firebase.google.com.
    
    Далее при небольших манипуляциях дажи название БД,
    + настроили регистрацию через email.
    
    Затем подготовили страницу регистрации/аутентификации
    пользовтеля + подготовли немного функционала.

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,30 @@ import './assets/main.css'
 import 'primeflex/primeflex.css'
 import 'primeicons/primeicons.css'
 
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura';
 import Menubar from 'primevue/menubar';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import { initializeApp } from "firebase/app";
+
 
 import App from './App.vue'
 import router from './router'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDiEP5WMExpgXE9RyKd35dDjAXVLCUIczA",
+    authDomain: "interviewlist-11.firebaseapp.com",
+    projectId: "interviewlist-11",
+    storageBucket: "interviewlist-11.appspot.com",
+    messagingSenderId: "680036692440",
+    appId: "1:680036692440:web:6e89f68165d45b48db42ad"
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
 
@@ -21,5 +37,8 @@ app.use( PrimeVue, {
     }
 })
 app.component('app-menubar', Menubar)
+app.component('app-button', Button)
+app.component('app-input-text', InputText)
+
 
 app.mount('#app')
--- src/views/PageAuth.vue
+++ src/views/PageAuth.vue
@@ -1,13 +1,59 @@
 <template>
-    <div>
-        AUTH
+    <app-toast position="bottom-right" />
+    <div class="flex justify-content-center p-2">
+        <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+            <div class="text-center mb-3">
+                <div class="text-900 text-3xl font-medium mb-3">Приветствую!</div>
+                <span class="text-600 font-medium line-height-3">{{ subtitleText }}</span>
+                <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer" @click="toggleAuth">
+                    {{ linkAccountText }}
+                </a>
+            </div>
+
+            <form @submit.prevent="submitForm">
+                <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+                <app-input-text v-model="email" id="email1" type="email" class="w-full mb-3" />
+
+                <label for="password1" class="block text-900 font-medium mb-2">Пароль</label>
+                <app-input-text v-model="password" id="password1" type="password" class="w-full mb-3" />
+
+                <app-button 
+                    :label="submitButtonText" 
+                    type="submit" icon="pi pi-user" 
+                    :loading="isLoading"
+                    class="w-full"></app-button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 
+const isLogin = ref<boolean>(true);
+const email = ref<string>('');
+const password = ref<string>('');
+const isLoading = ref<boolean>(false);;
+
+const toggleAuth = () => {
+    isLogin.value = !isLogin.value;
+}
+
+const subtitleText = computed<string>(() => {
+    return isLogin.value ? 'Аккаунта еще нет' : 'Уже есть аккаунт';
+});
+
+const linkAccountText = computed<string>(() => {
+    return isLogin.value ? 'Создайте сейчас' : 'Войдите в него';
+});
+
+const submitButtonText = computed<string>(() => {
+    return isLogin.value ? 'Регистрация' : 'Вход';
+});
+
+const submitForm = ():void =>{
+    console.log('test');
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
