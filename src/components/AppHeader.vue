7a63e0a75aa2f160778f4ba1832c45d2830a4bb0
GummerD <denis_gummer@mail.ru>
2024-08-06 11:05:07 +0300

    1. Настройка верхнего меню. Авторизация.
    Дополнительно к ранее оформленному компонету AppHeader,
    в которм использовался компопнет primevue - mrnubar,
    добавили логику условной отрисовки пунктов меню напрвленную
    на проверкку авторизации.
    
    Для этих целей воспользовлись внешним хрнилищем - Pinia,
    в которм предусмотрели модель - userId.
    
    На данном этапе все воспроизведено в упрощенном варианте для
    целей понимания и отслеживания реактивности, работы приложения.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,22 +1,39 @@
 <template>
     <app-menubar class="menu" :model="items">
         <template #item="{ item, props }">
-            <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
-                <span :class="item.icon"></span>
-                <span class="ml-2"> {{ item.label }}</span>
-            </router-link>
+            <template v-if="item.show">
+                <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
+                    <span :class="item.icon"></span>
+                    <span class="ml-2"> {{ item.label }}</span>
+                </router-link>
+            </template>
+        </template>
+        <template #end>
+            <span 
+                v-if="userStore.userId" 
+                class="flex align-item-center menu_exit"
+                @click="userStore.userId = ''"
+                >
+                <span class="pi pi-sign-out p-p-menuitem-icon"></span>
+                <span class="ml-2">Выход</span>
+            </span>
         </template>
     </app-menubar>
 </template>
 
 <script setup lang="ts">
 
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useUserStore } from '@/stores/useUserStore';
+import type { ComputedRef } from 'vue';
+
+const userStore = useUserStore();
 
 interface IMenueItem {
     label: string,
     icon: string,
-    path: string
+    path: string,
+    show: ComputedRef<boolean>
 }
 
 const items = ref<IMenueItem[]>([
@@ -24,21 +41,25 @@ const items = ref<IMenueItem[]>([
         label: "Авторизация",
         icon: 'pi pi-user',
         path: '/auth',
+        show: computed((): boolean => !userStore.userId)
     },
     {
         label: "Добавить",
         icon: 'pi pi-plus',
-        path: '/add'
+        path: '/add',
+        show: computed((): boolean => !!userStore.userId)
     },
     {
         label: "Список собеседований",
         icon: 'pi pi-list',
-        path: '/list'
+        path: '/list',
+        show: computed((): boolean => !!userStore.userId)
     },
     {
         label: "Статистика",
         icon: 'pi pi-chart-pie',
-        path: '/statistic'
+        path: '/statistic',
+        show: computed((): boolean => !!userStore.userId)
     },
 
 ])
